33c8a190b53d53dbd2cf54ff51b4f9945a530c95
Dmitry Sidorenko <d.sidorenko@oversun-media.com>
2013-08-12 15:38:02 +0300

    upd Animation Editor

--- js/controls/pages/animationEditor/animationEditor.js
+++ js/controls/pages/animationEditor/animationEditor.js
@@ -13,10 +13,11 @@ define(['knockout', 'BaseControl', 'sprite'], function(ko, BaseControl, Sprite){
 		this.id = ko.observable(1);
 		this.name = ko.observable('Эффект рассеивания');
 		this.graphicResource = ko.observable('http://jira.dev.playrooms.ru/secure/attachment/21204/sprite.png');
+		// this.graphicResource = ko.observable('http://2.bp.blogspot.com/-mD_cX5gZNAE/TaO74Og4F7I/AAAAAAAAAio/umyTQmA7I80/s200/DawnWalkSide_Dict.png');
 		this.frameStart = ko.observable(0);
 		this.frameCount = ko.observable(15);
 		this.frameWidth = ko.observable(92);
-		this.frameHeight = ko.observable(80);
+		this.frameHeight = ko.observable(83);
 		this.rowsCount = ko.observable(1);
 		this.colsCount = ko.observable(15);
 		this.time = ko.observable();
--- js/knockoutBindings/animate.js
+++ js/knockoutBindings/animate.js
@@ -11,16 +11,78 @@
 	    };
 	})();
 
+
+	function rowFromIndex(index, colCount){
+		return index / colCount | 0;
+	}
+	function colFromIndex(index, colCount){
+		return index - rowFromIndex(index, colCount) * colCount;
+	}
+
 	
+	function loadSprite(src, options, model){
+		var def = $.Deferred();
+		var sprite = new Sprite(src, {
+			  frameW: model.frameWidth(),
+			  frameH: model.frameHeight(),
+			  useTimer:false,
+			  startCol:colFromIndex(model.frameStart(), model.colsCount()),
+			  startRow:rowFromIndex(model.frameStart(), model.colsCount()),
+			  endCol:colFromIndex(model.frameCount() - 1, model.colsCount()),
+			  endRow:rowFromIndex(model.frameCount() - 1, model.colsCount()),
+			  interval:1000/(options.fps() || 10),
+			  postInitCallback: function() { // Runs when the sprite is ready.
+			    // Start animating.
+			    def.resolve();
+			  }
+			});
+
+		return {sprite:sprite, promise:def.promise()};
+	}
 
 
-	var def = $.Deferred();
     define(['jquery', 'knockout', 'sprite'], function($, ko, Sprite){
 		ko.bindingHandlers.animate = {
 			init:function(el, valueAccessor, allBindingsAccessor, viewModel, bindingContext){
 				var options = ko.utils.unwrapObservable(valueAccessor());
 				var context = $(el)[0].getContext('2d');
-				
+
+				var model = options.model;
+				var res = model.graphicResource();
+
+
+				var load = loadSprite(res, options, model);
+				var def = load.promise;
+				var sprite = load.sprite;
+
+				var animationData = {
+					sprite:sprite,
+					isRun:true,
+					context:context,
+					startAnimation:function(){
+						this.isRun = true;
+						this.animate();
+						this.sprite.startLoop();
+					},
+					stopAnimation:function(){
+						this.isRun = false;
+						this.sprite.stopLoop();
+					},
+					animate:function(){ // Animation loop that draws the canvas
+						var context = this.context;
+						if(this.isRun){
+						  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clear the canvas
+						  this.sprite.draw(context, 0, 0); // Draw the sprite
+						  requestAnimFrame(this.animate.bind(this)); // Run the animation loop
+						}
+				}
+				};
+
+				def.done(function(){
+					animationData.startAnimation();
+				});
+
+				$(el).data('_animationData', animationData);
 			},
 			update:function(el, valueAccessor, allBindingsAccessor, viewModel, bindingContext){
 				var options = ko.utils.unwrapObservable(valueAccessor());
@@ -29,37 +91,25 @@
 				var model = options.model;
 				var res = model.graphicResource();
 
-
-				var context = $(el)[0].getContext('2d');
-				var sprite = new Sprite(res, {
-					  frameW: model.frameWidth(),
-					  frameH: model.frameHeight(),
-					  useTimer:false,
-					  startCol:0,
-					  startRow:0,
-					  endCol:14,
-					  endRow:0,
-					  interval:1000/(options.fps() || 10),
-					  postInitCallback: function() { // Runs when the sprite is ready.
-					    // Start animating.
-					    def.resolve();
-					  }
+				var animationData = $(el).data('_animationData');
+				var sprite = animationData.sprite;
+				sprite.frameW = model.frameWidth();
+				sprite.frameH = model.frameHeight();
+				sprite.useTimer = false;
+				sprite.startCol = colFromIndex(model.frameStart(), model.colsCount());
+				sprite.startRow = rowFromIndex(model.frameStart(), model.colsCount());
+				sprite.endCol = colFromIndex(model.frameCount(), model.colsCount());
+				sprite.endRow = rowFromIndex(model.frameCount(), model.colsCount());
+				sprite.interval = 1000/(options.fps() || 10);
+				if(sprite.sourceFile !== res){
+					animationData.stopAnimation();
+					var load = loadSprite(res, options, model);
+					var def = load.promise;
+					var sprite = load.sprite;
+					animationData.sprite = sprite;
+					def.done(function(){
+						animationData.startAnimation();
 					});
-				def.done(function(){
-					//setInterval(draw.bind(sprite, context, sprite), 1000/80);
-					run();
-				   	animate();
-				});
-
-				function run(){
-			    	sprite.startLoop();
-
-				}
-
-				function animate() { // Animation loop that draws the canvas
-				  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clear the canvas
-				  sprite.draw(context, 0, 0); // Draw the sprite
-				  requestAnimFrame(animate); // Run the animation loop
 				}
 			}
 		};
